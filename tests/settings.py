96ef3843e1a84e542971d46c9e5c0601cce64f41
Adam Jakubowski <ajakubo1@gmail.com>
2024-10-14 12:39:02 +0200

    style

--- tests/settings.py
+++ tests/settings.py
@@ -1,35 +1,35 @@
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'betterforms-tests.db',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "betterforms-tests.db",
     },
 }
 
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.admin',
-    'django.contrib.contenttypes',
-    'django.contrib.messages',
-    'django.contrib.sessions',
-    'django.contrib.staticfiles',
-    'betterforms',
-    'tests',
+    "django.contrib.auth",
+    "django.contrib.admin",
+    "django.contrib.contenttypes",
+    "django.contrib.messages",
+    "django.contrib.sessions",
+    "django.contrib.staticfiles",
+    "betterforms",
+    "tests",
 )
 
 DEBUG = True
 
-SECRET_KEY = 'JVpuGfSgVm2IxJ03xArw5mwmPuYEzAJMbhsTnvLXOPSQR4z93o'
+SECRET_KEY = "JVpuGfSgVm2IxJ03xArw5mwmPuYEzAJMbhsTnvLXOPSQR4z93o"
 
 SITE_ID = 1
 
-ROOT_URLCONF = 'tests.urls'
+ROOT_URLCONF = "tests.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'APP_DIRS': True,
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
@@ -42,13 +42,13 @@ TEMPLATES = [
 ]
 
 MIDDLEWARE = [
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
 ]
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 USE_TZ = False
--- tests/urls.py
+++ tests/urls.py
@@ -8,11 +8,15 @@ from .forms import Step1Form, Step2Form
 class TestWizardView(SessionWizardView):
     def done(self, form_list, **kwargs):
         context = {
-            'form_list': form_list,
+            "form_list": form_list,
         }
         return self.render_to_response(context)
 
 
 urlpatterns = [
-    path('test-wizard-view/', TestWizardView.as_view([Step1Form, Step2Form]), name='test_wizard'),
+    path(
+        "test-wizard-view/",
+        TestWizardView.as_view([Step1Form, Step2Form]),
+        name="test_wizard",
+    ),
 ]
